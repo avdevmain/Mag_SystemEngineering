ee99878114731078e3ce2a7fc07fca1762431f83
Anton <antonvolegov@mail.ru>
2021-12-06 15:30:57 +0300

    Add files via upload

---
+++ Практика6/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace Практика6
+{
+
+    //No IoC
+/*
+    class Program
+    {       
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+
+            player.TriggerAttack();
+        }
+    }
+   class Weapon
+   {
+       public void Attack()
+       {
+           Console.WriteLine("Bang!");
+       }
+   }
+
+   class Player
+   {
+       private Weapon weapon = new Weapon();
+
+        public void TriggerAttack()
+        {
+            weapon.Attack();
+        }
+   }
+*/
+
+
+//With IoC
+///*
+    class Program
+    {       
+        static void Main(string[] args)
+        {
+            Player player = new Player(null);
+            player.TriggerAttack();
+
+            player.SetWeapon(new Gun());
+            player.TriggerAttack();
+
+            player.SetWeapon(new Shotgun());
+            player.TriggerAttack();
+
+        }
+    }
+    interface IWeapon
+    {
+        void Attack();
+    }
+
+   class Gun : IWeapon
+   {
+        public void Attack()
+        {
+           Console.WriteLine("Bang!");
+        }
+   }
+
+   class Shotgun : IWeapon
+   {
+       public void Attack()
+       {
+           Console.WriteLine("Boom!");
+       }
+   }
+
+   class Player
+   {
+       IWeapon weapon;
+        public Player(IWeapon weaponToSet)
+        {
+            if (weaponToSet!=null)
+                this.weapon = weaponToSet;
+        }
+
+        public void SetWeapon(IWeapon weaponToSet)
+        {
+            this.weapon = weaponToSet;
+        }
+
+        public void TriggerAttack()
+        {
+            if (weapon!=null)
+                weapon.Attack();
+            else
+                Console.WriteLine("Punch!");
+        }
+   }
+
+//*/
+    
+}
